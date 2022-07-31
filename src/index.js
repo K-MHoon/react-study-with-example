05d4cc28fa6b73f399cf120e3baffb453ca5852e
K-MHOON <azmria117@gmail.com>
2022-08-01 00:42:27 +0900

    feat: 컴포넌트 간에 state 공유하여 온도 화씨 섭씨 표현하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <Calculator />
   </React.StrictMode>,
 );
 
