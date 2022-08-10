98ddc80880e149d61f9779574c94224452704bba
K-MHOON <azmria117@gmail.com>
2022-08-10 22:33:49 +0900

    feat: CSSModule 활용

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import SassComponent from "./chapter_21/SassComponent";
+import CSSModule from "./chapter_21/CSSModule";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <SassComponent />
+    <CSSModule />
   </React.StrictMode>
 );
 
