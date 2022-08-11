35aa288e7115930ebf8c6d162c70d460a47a0134
K-MHOON <azmria117@gmail.com>
2022-08-12 00:11:28 +0900

    feat: MiniProject Todo List 개발 Template, Input 구현

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./chapter_22/App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import StyledComponent from "./chapter_21/StyledComponent";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <StyledComponent />
+    <App />
   </React.StrictMode>
 );
 
