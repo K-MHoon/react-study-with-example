ebdf95d6bbc6018a6c544e23a88649329d92b31d
K-MHOON <azmria117@gmail.com>
2022-08-16 12:19:14 +0900

    feat: axios 활용하여 뉴스 api 호출하기 구현

---
+++ src/chapter_25/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=7b9d7e51e7c34b188c0c51d48362a688"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
+      )}
+    </div>
+  );
+};
+
+export default App;
