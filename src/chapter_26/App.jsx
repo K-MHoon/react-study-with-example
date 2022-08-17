e5aaf6f310447280db3c1ec36d7b308c893028f6
K-MHOON <azmria117@gmail.com>
2022-08-17 21:32:30 +0900

    feat: Context 동적으로 활용하기

--- src/chapter_26/App.jsx
+++ src/chapter_26/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
-import ColorContext from "./contexts/ColorContext";
+import ColorContext, { ColorProvider } from "./contexts/ColorContext";
 
 const App = () => {
   return (
-    <ColorContext.Provider>
+    <ColorProvider>
       <div>
         <ColorBox />
       </div>
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 };
 
--- src/chapter_26/components/ColorBox.jsx
+++ src/chapter_26/components/ColorBox.jsx
@@ -4,14 +4,23 @@ import ColorContext from "../contexts/ColorContext";
 const ColorBox = () => {
   return (
     <ColorContext.Consumer>
-      {(value) => (
-        <div
-          style={{
-            width: "64px",
-            height: "64px",
-            background: value.color,
-          }}
-        />
+      {({ state }) => (
+        <>
+          <div
+            style={{
+              width: "64px",
+              height: "64px",
+              background: state.color,
+            }}
+          />
+          <div
+            style={{
+              width: "32px",
+              height: "32px",
+              background: state.subcolor,
+            }}
+          />
+        </>
       )}
     </ColorContext.Consumer>
   );
