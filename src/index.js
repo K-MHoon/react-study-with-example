10fbc38c13ad6c29edd2ab60a689674faccb909c
K-MHOON <azmria117@gmail.com>
2022-08-20 19:38:45 +0900

    feat: store 구현 및 리덕스 DevTools 라이브러리 추가 및 적용

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./chapter_27/App";
 import "./index.css";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reportWebVitals from "./reportWebVitals";
+import { legacy_createStore as createStore } from "redux";
+import rootReducer from "./chapter_27/modules";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <App />
-  </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
