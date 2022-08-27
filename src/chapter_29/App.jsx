30fd7ccab67d7365cebe7cc72313ef31c408c4f1
K-MHOON <azmria117@gmail.com>
2022-08-27 21:31:24 +0900

    feat: loadable compoent 변경  및 preload() 사용하여 선 로딩 구현

--- src/chapter_29/App.jsx
+++ src/chapter_29/App.jsx
@@ -1,22 +1,29 @@
 import React, { useState, Suspense } from "react";
 import logo from "../logo.svg";
 import "../App.css";
-const SplitMe = React.lazy(() => import("./SplitMe"));
+import loadable from "@loadable/component";
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div> loadble component loading...</div>,
+});
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    // 마우스를 올리는 것만으로도 로딩이 시작된다.
+    SplitMe.preload();
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
