0310bd67cefbf4584e0ffa95de559ff52f3563a1
K-MHOON <azmria117@gmail.com>
2022-08-29 00:33:43 +0900

    feat: 기본 컴포넌트 구성

--- src/chapter_30/App.jsx
+++ src/chapter_30/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import BluePage from "./pages/BluePage";
+import RedPage from "./pages/RedPage";
+import Menu from "./components/Menu";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Menu />
+      <hr />
+      <Routes>
+        <Route path="/red" element={<RedPage></RedPage>} />
+        <Route path="/blue" element={<BluePage></BluePage>} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import App from "./chapter_29/App";
+import App from "./chapter_30/App";
 import "./index.css";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reportWebVitals from "./reportWebVitals";
@@ -25,9 +25,12 @@ sagaMiddleware.run(rootSaga);
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <Provider store={store}>
+  // <Provider store={store}>
+  //   <App />
+  // </Provider>
+  <BrowserRouter>
     <App />
-  </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
