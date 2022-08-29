13f19166f331664b2d2a986a403cf96037336746
K-MHOON <azmria117@gmail.com>
2022-08-30 00:34:29 +0900

    feat: express 활용하여 node 서버 구축, jsx to js 로 수정

--- src/chapter_30/index.server.js
+++ src/chapter_30/index.server.js
@@ -1,5 +1,25 @@
 import ReactDOMServer from "react-dom/server";
+import express from "express";
+import { StaticRouter } from "react-router-dom/server";
+import App from "./App";
 
-const html = ReactDOMServer.renderToString(<div>Hello Server Rendering!</div>);
+const app = express();
 
-console.log(html);
+// 서버 사이드 렌더링을 처리할 핸들러 함수
+const serverRender = (req, res, next) => {
+  // 이 함수는 404가 떠야 하는 상황에 서버 사이드 렌더링을 해준다.
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  const root = ReactDOMServer.renderToString(jsx);
+  res.send(root);
+};
+
+app.use(serverRender);
+
+app.listen(5000, () => {
+  console.log("Running on http://localhost:5000");
+});
