6971241a22cd5933c1edb8a322146c364dcfd132
K-MHOON <azmria117@gmail.com>
2022-08-30 22:37:28 +0900

    feat: express static 활용하여 build 정적 css 정보 가져오기

--- src/chapter_30/index.server.js
+++ src/chapter_30/index.server.js
@@ -2,6 +2,44 @@ import ReactDOMServer from "react-dom/server";
 import express from "express";
 import { StaticRouter } from "react-router-dom/server";
 import App from "./App";
+import path from "path";
+import fs from "fs";
+
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve("./build/asset-manifest.json"), "utf8")
+);
+
+const chunks = Object.keys(manifest.files)
+  .filter((key) => /chunk\.js$/.exec(key))
+  .map((key) => `<script src="${manifest.files[key]}"></script>`)
+  .join("");
+
+function createPage(root) {
+  return `<!doctype html>
+  <html lang="en">
+  <head>
+  <meta charset="utf-8"/>
+  <link rel="shortcut icon" href="/favicon.ico"/>
+  <meta
+   name="viewport" 
+   content="width=device-width,initial-scale=1,shrink-to-fit=no"
+  />
+   <meta name="theme-color" content="#000000"/>
+   <meta name="description" content="Web site created using create-react-app"/>
+   <title>React App</title>
+   <link href="${manifest.files["main.css"]}" rel="stylesheet">
+   </head>
+   <body>
+   <noscript>You need to enable JavaScript to run this app.</noscript>
+   <div id="root">
+    ${root}
+   </div>
+   ${chunks}
+   <script defer="defer" src="${manifest.files["main.js"]}"></script>
+   </body>
+   </html>
+  `;
+}
 
 const app = express();
 
@@ -15,9 +53,15 @@ const serverRender = (req, res, next) => {
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx);
-  res.send(root);
+  res.send(createPage(root));
 };
 
+// static 미들웨어를 활용하여, 서버를 통해 build에 있는 js, css 정적파일에 접근
+const serve = express.static(path.resolve("./build"), {
+  index: false,
+});
+
+app.use(serve);
 app.use(serverRender);
 
 app.listen(5000, () => {
