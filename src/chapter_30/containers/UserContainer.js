b3a678aefd73eb0303a5e40b5a5d39e905fb76c0
K-MHOON <azmria117@gmail.com>
2022-09-04 09:23:02 +0900

    feat: usePreloader Custom Hook 제작하여 적용

--- src/chapter_30/containers/UserContainer.js
+++ src/chapter_30/containers/UserContainer.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import User from "../components/User";
-import { Preloader } from "../../lib/PreloadContext";
+import { Preloader, usePreloader } from "../../lib/PreloadContext";
 import { getUser } from "../modules/users";
 import { useParams } from "react-router-dom";
 
@@ -10,14 +10,13 @@ const UserContainer = () => {
   const user = useSelector((state) => state.users.user);
   const dispatch = useDispatch();
 
+  usePreloader(() => dispatch(getUser(id)));
   useEffect(() => {
     if (user && user.id === parseInt(id, 10)) return; // 사용자가 존재하고, id가 일치한다면 요청하지 않음
     dispatch(getUser(id));
   }, [dispatch, id, user]); // id가 바뀔 때 새로 요청해야 한다.
 
-  if (!user) {
-    return <Preloader resolve={() => dispatch(getUser(id))}></Preloader>;
-  }
+  if (!user) return null;
   return <User user={user} />;
 };
 
