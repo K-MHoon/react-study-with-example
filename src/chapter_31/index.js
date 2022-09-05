e8e53d97ef644875c5aa907eedaebc974bbb742d
K-MHOON <azmria117@gmail.com>
2022-09-05 21:43:10 +0900

    feat: koa-router, nodenom 라이브러리 간단 활용

--- src/chapter_31/index.js
+++ src/chapter_31/index.js
@@ -1,6 +1,18 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx) => {
+  ctx.body = '홈';
+});
+
+router.get('/about', (ctx) => {
+  ctx.body = '소개';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.use(async (ctx, next) => {
   console.log(ctx.url);
@@ -19,7 +31,7 @@ app.use((ctx, next) => {
 });
 
 app.use((ctx) => {
-  ctx.body = 'hello world';
+  ctx.body = 'hello world - nodemon using';
 });
 
 app.listen(4000, () => {
