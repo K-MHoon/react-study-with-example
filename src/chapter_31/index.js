2654fcd3d65c8f12cdb2695b3e7900b784d3d38e
K-MHOON <azmria117@gmail.com>
2022-09-06 19:56:16 +0900

    feat: router, HTTP Method 활용하여 api 테스트

--- src/chapter_31/index.js
+++ src/chapter_31/index.js
@@ -1,46 +1,14 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx) => {
-  ctx.body = '홈';
-});
-
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  // name 파라미터가 있는지 없는지에 따라 다르게 출력
-  ctx.body = name ? `${name}의 소개` : '소개';
-});
-
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  // id 파라미터 유무ㅡ에 따라 다르게 결과 출력
-  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
-});
+router.use('/api', api.routes()); // api 라우트 적용
 
 app.use(router.routes()).use(router.allowedMethods());
-
-// app.use(async (ctx, next) => {
-//   console.log(ctx.url);
-//   console.log(1);
-//   if (ctx.query.authorized !== '1') {
-//     ctx.status = 401;
-//     return;
-//   }
-//   await next();
-//   console.log('END');
-// });
-
-// app.use((ctx, next) => {
-//   console.log(2);
-//   next();
-// });
-
-// app.use((ctx) => {
-//   ctx.body = 'hello world - nodemon using';
-// });
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
