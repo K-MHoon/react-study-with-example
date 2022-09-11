40fda547bcb7bd22a86bac15b38d36ed43935ded
K-MHOON <azmria117@gmail.com>
2022-09-12 00:43:55 +0900

    feat: 토큰 재발급 기능 구현

--- src/lib/jwtMiddleware.js
+++ src/lib/jwtMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import User from '../chapter_31/models/user';
 
-const jwtMiddleware = (ctx, next) => {
+const jwtMiddleware = async (ctx, next) => {
   const token = ctx.cookies.get('access_token');
   if (!token) return next();
   try {
@@ -9,7 +10,16 @@ const jwtMiddleware = (ctx, next) => {
       _id: decoded._id,
       username: decoded.username,
     };
-    console.log(decoded);
+    // 토큰의 남은 유효 기간이 1일 미만이면 재발급
+    const now = Math.floor(Date.now() / 1000);
+    if (decoded.exp - now < 60 * 60 * 24 * 1) {
+      const user = await User.findById(decoded._id);
+      const token = user.generateToken();
+      ctx.cookies.set('access_token', token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+        httpOnly: true,
+      });
+    }
     return next();
   } catch (e) {
     // 토큰 검증 실패
